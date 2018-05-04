db7ee0154bf0522194445983d57a6ba7aa7c2544
Crystal <git@cryb.org>
2018-05-03 20:55:29 -0400

    Solution to problem 10.

---
+++ python/problem-010.py
@@ -0,0 +1,24 @@
+# Problem 10 - Summation of primes
+#
+# The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+# Find the sum of all the primes below two million.
+
+n = 2000000
+result = 0
+primes = list()
+
+# This builds a list of all primes less than n.
+for i in range(2, n):
+    for prime in primes:
+        if i % prime == 0:
+            break
+    else:
+        print(i)
+        result += i
+        primes.append(i)
+
+print("Problem 10: " + str(result))
+
+# Okay, I really liked this method of collecting all the primes in a list until
+# this problem (came up with in problem 3, and used in 7 too). This got the
+# right answer, but took about 15 minutes to run. May need to revisit this.
