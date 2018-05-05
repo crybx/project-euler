8a08917f917f6770ca1eb2e426e8d72f5d6ba6df
Crystal <git@cryb.org>
2018-05-05 00:15:57 -0400

    Added timing.

--- python/problem-010.py
+++ python/problem-010.py
@@ -3,6 +3,9 @@
 # The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 # Find the sum of all the primes below two million.
 
+import time
+
+start_time = time.time()
 n = 2000000
 result = 0
 primes = list()
@@ -14,11 +17,14 @@ for i in range(2, n):
             break
     else:
         print(i)
-        result += i
         primes.append(i)
 
+result = sum(primes)
 print("Problem 10: " + str(result))
+print("{} ms".format(round(1000 * (time.time() - start_time))))
 
 # Okay, I really liked this method of collecting all the primes in a list until
 # this problem (came up with in problem 3, and used in 7 too). This got the
 # right answer, but took about 15 minutes to run. May need to revisit this.
+
+# 2nd run: 918931 ms = 15.3 minutes on a laptop
