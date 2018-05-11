b7a221faaaec3b304a77dc13f359f0671784f0e4
crybx <crybx@users.noreply.github.com>
2018-05-10 20:41:38 -0400

    Solution for problem 20.

---
+++ python/problem_020.py
@@ -0,0 +1,18 @@
+# Problem 20 - Factorial digit sum
+#
+# n! means n * (n − 1) * ... * 3 * 2 * 1
+#
+# For example, 10! = 10 * 9 * ... * 3 * 2 * 1 = 3628800,
+# and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+#
+# Find the sum of the digits in the number 100!
+
+import math
+
+number = math.factorial(100)
+
+digits = str(number)
+digits = list(map(int, digits))
+digit_sum = sum(digits)
+
+print("Problem 20: " + str(digit_sum))
